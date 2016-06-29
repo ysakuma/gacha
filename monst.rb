e039aa4a3d9c7213a82d8a0ce159910ef1837d3c
ysakuma <ysakuma@tk2-220-19912.vs.sakura.ne.jp>
2016-06-29 19:14:33 +0900

    view probability

--- monst.rb
+++ monst.rb
@@ -7,7 +7,7 @@ CONFIG    = YAML.load_file("#{File.expand_path(File.dirname($0))}/config.yml")
 THRESHOLD = ($*[0] || 15).to_i
 
 def send_slack
-  cmd = "curl -s -X POST --data-urlencode 'payload={\"channel\": \"#monst-gacha\", \"text\": \"<\!here> 今超大だよ\"}' #{CONFIG['slack_api_url']}"
+  cmd = "curl -s -X POST --data-urlencode 'payload={\"channel\": \"#monst-gacha\", \"text\": \"<\!here> 今超大だよ！ちなみに #{@probability}% ね。 \"}' #{CONFIG['slack_api_url']}"
   `#{cmd}`
 end
 
@@ -16,5 +16,6 @@ def send_api
 end
 
 gacha_doc = Nokogiri.Slop(open('http://monnsutogatya.com').read)
-result = gacha_doc.at_css('#a-box table.report-font tr:nth-child(5) font.text-color2').text.to_i > THRESHOLD
+@probability = gacha_doc.at_css('#a-box table.report-font tr:nth-child(5) font.text-color2').text
+result = @probability.to_i > THRESHOLD
 send_api if result
